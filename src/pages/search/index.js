11c1996ea7ac5554ce867122e603332bb6ecf0b5
miello <partearth1212@gmail.com>
2021-04-24 21:08:39 +0700

    Create templete of create, edit, search, landing

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -2,9 +2,16 @@ import { Box, Typography } from '@material-ui/core'
 import { NavBar } from 'components'
 import { AuthContext } from 'context/userContext'
 import React, { useContext } from 'react'
+import { useHistory } from 'react-router'
 
 function SearchResult({ name }) {
   const userState = useContext(AuthContext)
+  const history = useHistory()
+
+  if (name === '') {
+    history.push('/')
+    return null
+  }
 
   return (
     <React.Fragment>
